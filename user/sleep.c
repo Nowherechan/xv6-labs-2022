a34bdf4fcd3ab4b592ba3191d0450d783c26b1ea
Nowherechan <zzxy233@gmail.com>
2022-12-14 19:04:05 +0800

    finished lab-util sleep

---
+++ user/sleep.c
@@ -0,0 +1,15 @@
+#include <kernel/types.h>
+#include <user/user.h>
+
+int
+main(int argc, char *argv[])
+{
+  if (argc != 2) {
+    printf("Usage: sleep <ticks>, where ticks is a integer\n");
+    exit(-1);
+  } else {
+    int ticks = atoi(argv[1]);
+    sleep(ticks);
+  }
+  exit(0);
+}
